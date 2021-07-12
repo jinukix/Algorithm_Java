225cd5640aa3b8ee0f5ffe988ecbfa3378f98ac7
Jinuk <wfo3977@gmail.com>
2021-07-12 17:29:39 +0900

    21.07.12

--- programmers/_2016년/Solution.java
+++ programmers/_2016년/Solution.java
@@ -1,2 +1,24 @@
-package _2016년;public class Solution {
+package _2016년;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, a-1, b);
+        Date date = cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("E", Locale.ENGLISH);
+
+        answer = sdf.format(date).toUpperCase();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(5, 24));
+    }
 }
--- programmers/파일명_정렬/Solution.java
+++ programmers/파일명_정렬/Solution.java
@@ -1,2 +1,32 @@
-package 파일명_정렬;public class Solution {
+package 파일명_정렬;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution {
+    public String[] solution(String[] files) {
+        Comparator<String> f = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String header1 = o1.split("[0-9]{1,5}")[0].toLowerCase();
+                String header2 = o2.split("[0-9]{1,5}")[0].toLowerCase();
+
+                if (header1.equals(header2)) {
+                    Pattern pattern = Pattern.compile("[0-9]{1,5}");
+                    Matcher matcher1 = pattern.matcher(o1);
+                    Matcher matcher2 = pattern.matcher(o2);
+
+                    if (matcher1.find() && matcher2.find()) {
+                        return Integer.compare(Integer.parseInt(matcher1.group()), Integer.parseInt(matcher2.group()));
+                    }
+                }
+                return CharSequence.compare(header1, header2);
+            }
+        };
+
+        Arrays.sort(files, f);
+        return files;
+    }
 }
