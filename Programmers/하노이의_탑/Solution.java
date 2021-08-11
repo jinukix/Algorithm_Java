bcd501908b8d3d17bfcdf8df3996c420b85b18c4
Jinuk <wfo3977@gmail.com>
2021-08-12 03:32:23 +0900

    21.08.12

---
+++ Programmers/하노이의_탑/Solution.java
@@ -0,0 +1,28 @@
+package 하노이의_탑;
+
+import java.util.ArrayList;
+
+class Solution {
+    ArrayList<int[]> list = new ArrayList<>();
+
+    public void hanoi(int count ,int start, int middle, int end) {
+        if (count == 1) {
+            list.add(new int[] {start, end});
+        } else {
+            hanoi(count-1, start, end, middle);
+            list.add(new int[] {start, end});
+            hanoi(count-1, middle, start, end);
+        }
+    }
+
+    public int[][] solution(int n) {
+        hanoi(n, 1, 2,3);
+
+        int[][] result = new int[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
