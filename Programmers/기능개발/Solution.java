1df3018f2afdd65258c021489e9d2f973009236d
Jinuk <wfo3977@gmail.com>
2021-08-14 00:30:39 +0900

    21.08.14

---
+++ Programmers/기능개발/Solution.java
@@ -0,0 +1,40 @@
+package 기능개발;
+
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int n = 0;
+        int time = 0;
+
+        for (int i = 0; i < progresses.length; i++) {
+            int t = (int) Math.ceil((100-progresses[i])/ (double)speeds[i]);
+
+            if (time < t) {
+                if (n!=0) list.add(n);
+                time = t;
+                n = 1;
+            } else {
+                n++;
+            }
+        }
+
+        list.add(n);
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] progresses = {93,30,55};
+        int[] speeds = {1,30,5};
+        sol.solution(progresses, speeds);
+    }
+}
---
+++ Programmers/타겟_넘버/Solution.java
@@ -0,0 +1,20 @@
+package 타겟_넘버;
+
+class Solution {
+    int result = 0;
+
+    public void dfs(int[] numbers, int target, int value, int idx) {
+        if (numbers.length == idx) {
+            if (value == target) result++;
+            return;
+        }
+
+        dfs(numbers, target, value+numbers[idx], idx+1);
+        dfs(numbers, target, value-numbers[idx], idx+1);
+    }
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return result;
+    }
+}
