55316ea852ea03885b1dd48f31b482d0fb9b0a2c
Jinuk <wfo3977@gmail.com>
2021-10-17 08:35:17 +0900

    21.10.17

---
+++ acmic/_10815_숫자_카드/Main.java
@@ -0,0 +1,34 @@
+package _10815_숫자_카드;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>(n);
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                bw.write(1 + " ");
+            } else {
+                bw.write(0 + " ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
---
+++ acmic/_10816_숫자_카드2/Main.java
@@ -0,0 +1,32 @@
+package _10816_숫자_카드2;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>(n);
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0)+1);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            bw.write(map.getOrDefault(key, 0) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
