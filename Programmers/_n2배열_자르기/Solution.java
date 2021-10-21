c5a4bf0c3d7c7338711680fa1bbe21a3343efb26
Jinuk <wfo3977@gmail.com>
2021-10-22 00:56:03 +0900

    21.10.22

---
+++ Programmers/_n2배열_자르기/Solution.java
@@ -0,0 +1,20 @@
+package _n2배열_자르기;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] result = new int[(int)(right-left)+1];
+        int idx = 0;
+        for (long i = left; i <= right; i++) {
+            result[idx++] = (int) (Math.max(i/n, i%n)+1);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(3, 2, 5)));
+    }
+}
