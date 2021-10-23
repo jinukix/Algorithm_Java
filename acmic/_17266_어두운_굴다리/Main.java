8ecd8389435ebd6f93b8c2515fd7bc95aea3fab8
Jinuk <wfo3977@gmail.com>
2021-10-24 00:39:40 +0900

    21.10.24

---
+++ acmic/_17266_어두운_굴다리/Main.java
@@ -0,0 +1,26 @@
+package _17266_어두운_굴다리;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int max = Math.max(temp[0], n-temp[m-1]);
+
+        for (int i = 1; i < temp.length-1; i++) {
+            max = Math.max(max, (temp[i+1] - temp[i]+1)/2);
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
