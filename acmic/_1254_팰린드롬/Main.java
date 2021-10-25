50f1d64a3c4ed4322f9ead831a5e31e7fd2e4c49
Jinuk <wfo3977@gmail.com>
2021-10-26 00:58:59 +0900

    21.10.26

---
+++ acmic/_1254_팰린드롬/Main.java
@@ -0,0 +1,25 @@
+package _1254_팰린드롬;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+
+        for (int i = 0; i <= input.length(); i++) {
+            StringBuilder sb = new StringBuilder(input);
+            sb.append(new StringBuilder(input.substring(0, i)).reverse());
+
+            if (sb.toString().equals(sb.reverse().toString())) {
+                bw.write(String.valueOf(sb.length()));
+                break;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
