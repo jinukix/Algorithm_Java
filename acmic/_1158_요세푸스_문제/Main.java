83006dd4345889397ed5523c30ac353c9b4dfe1a
Jinuk <wfo3977@gmail.com>
2021-10-30 00:12:40 +0900

    21.10.30

---
+++ acmic/_1158_요세푸스_문제/Main.java
@@ -0,0 +1,36 @@
+package _1158_요세푸스_문제;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder("<");
+
+        while (queue.size() != 1) {
+            for (int i = 0; i < k-1; i++) {
+                queue.add(queue.poll());
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
