4eb56cd02d8ef37125150a5a085fb8e1cd84183a
Jinuk <wfo3977@gmail.com>
2021-11-01 00:38:02 +0900

    21.11.01

---
+++ acmic/_9012_괄호/Main.java
@@ -0,0 +1,40 @@
+package _9012_괄호;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            if (isValidPS(br.readLine())) {
+                bw.write("YES\n");
+            } else {
+                bw.write("NO\n");
+            }
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static boolean isValidPS(String s) {
+        int open = 0;
+        int close = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;
+            } else {
+                close++;
+                if (close > open) return false;
+            }
+        }
+
+        return open == close;
+    }
+}
