494af51883361076bb0fc2cc770fdfbab34f3e79
Jinuk <wfo3977@gmail.com>
2021-11-03 10:19:03 +0900

    21.11.03

---
+++ acmic/_1309_동물원/Main.java
@@ -0,0 +1,31 @@
+package _1309_동물원;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][3];
+        Arrays.fill(dp[1], 1);
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        bw.write(Integer.toString(Arrays.stream(dp[n]).sum() % 9901));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
