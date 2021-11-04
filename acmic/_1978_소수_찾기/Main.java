53f2a7601c05ef3693f4cc593354740eb383bbb3
Jinuk <wfo3977@gmail.com>
2021-11-04 12:05:28 +0900

    21.11.04

---
+++ acmic/_1978_소수_찾기/Main.java
@@ -0,0 +1,32 @@
+package _1978_소수_찾기;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPrime(Integer.parseInt(st.nextToken()))) result++;
+        }
+
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i <= (int)Math.sqrt(n); i++) {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
