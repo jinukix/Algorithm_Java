5c610e0be0470e2b4d485f118e7cad7f2485dfc6
Jinuk <wfo3977@gmail.com>
2021-11-05 03:46:00 +0900

    21.11.05

---
+++ acmic/_2217_로프/Main.java
@@ -0,0 +1,35 @@
+package _2217_로프;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        int cnt = 1;
+        int result = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int w = cnt++ * arr[i];
+            result = Math.max(result, w);
+        }
+
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
