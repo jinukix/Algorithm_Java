2e23931ab2dbcd330901fe69face92ad5a7913d0
Jinuk <wfo3977@gmail.com>
2021-11-07 02:15:41 +0900

    21.11.07

--- Programmers/게임_맵_최단거리/Solution.java
+++ Programmers/게임_맵_최단거리/Solution.java
@@ -4,14 +4,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    int[][] dir = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public void bfs(int[][] maps) {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {0, 0});
+        queue.add(new int[]{0, 0});
 
-        int y, x;
-        int dy, dx;
+        int y, x, dy, dx;
         while (!queue.isEmpty()) {
             int[] start = queue.poll();
             y = start[0];
@@ -21,18 +21,21 @@ class Solution {
                 dy = y + dir[i][0];
                 dx = x + dir[i][1];
 
-                if (0<=dy && dy<maps.length && 0<=dx && dx<maps[0].length && maps[dy][dx]==1) {
-                    maps[dy][dx] = maps[y][x]+1;
-                    queue.add(new int[] {dy, dx});
+                if (0 <= dy && dy < maps.length && 0 <= dx && dx < maps[0].length
+                    && maps[dy][dx] == 1) {
+                    maps[dy][dx] = maps[y][x] + 1;
+                    queue.add(new int[]{dy, dx});
                 }
             }
         }
-
     }
+
     public int solution(int[][] maps) {
         bfs(maps);
-        int result = maps[maps.length-1][maps[0].length-1];
-        if (result==0) result = -1;
+        int result = maps[maps.length - 1][maps[0].length - 1];
+        if (result == 1) {
+            result = -1;
+        }
         return result;
     }
 }
