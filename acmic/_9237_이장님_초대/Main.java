d3f78a268554925910a7fd3e6c16551e97d2fc5f
Jinuk <wfo3977@gmail.com>
2021-11-16 01:06:22 +0900

    21.11.16

--- acmic/_9237_이장님_초대/Main.java
+++ acmic/_9237_이장님_초대/Main.java
@@ -1,2 +1,33 @@
-package _9237_이장님_초대;public class Main {
+package _9237_이장님_초대;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        int result = 0;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            result = Math.max(result, arr[i] + arr.length - i + 1);
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
