fddf73d2ff358a4113e1d24bc58454686e05ecd7
Jinuk <wfo3977@gmail.com>
2021-11-18 02:32:15 +0900

    21.11.18

--- acmic/_1448_삼각형_만들기/Main.java
+++ acmic/_1448_삼각형_만들기/Main.java
@@ -1,6 +1,10 @@
-package _17266_어두운_굴다리;
+package _1448_삼각형_만들기;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Main {
@@ -10,16 +14,23 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
 
-        int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int max = Math.max(temp[0], n - temp[m - 1]);
-
-        for (int i = 1; i < temp.length - 1; i++) {
-            max = Math.max(max, (temp[i + 1] - temp[i] + 1) / 2);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        bw.write(String.valueOf(max));
+        Arrays.sort(arr);
+        int result = -1;
+
+        for (int i = n - 1; i >= 2; i--) {
+            if (arr[i] < arr[i - 1] + arr[i - 2]) {
+                result = arr[i] + arr[i - 1] + arr[i - 2];
+                break;
+            }
+        }
+
+        bw.write(result + "");
         bw.flush();
         bw.close();
         br.close();
